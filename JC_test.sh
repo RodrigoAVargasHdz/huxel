9ad45e3348d88902c23e9b06a5d71f8487117a6a
RodrigoAVargasHdz <ravh011@gmail.com>
2021-12-16 00:22:48 -0500

    SLRUM file to run

--- JC_test.sh
+++ JC_test.sh
@@ -5,13 +5,13 @@
 #SBATCH --mem=64G 
 #SBATCH --qos nopreemption  
 #SBATCH --partition=cpu 
-#SBATCH --job-name=huxel_n_50_l_0_exp_AdamW 
+#SBATCH --job-name=huxel_n_10_l_0_exp_AdamW 
 #SBATCH --time=24:00:00 
-#SBATCH --output=out_huxel_n_50_l_0_exp_AdamW.log 
+#SBATCH --output=out_huxel_n_10_l_0_exp_AdamW.log 
 
 source activate $HOME/huckel-jax 
-/h/rvargas/.conda/envs/huckel-jax/bin/python main.py --N 50 --lr 2E-2 --l 0 --batch_size 128 --job opt --beta exp
-/h/rvargas/.conda/envs/huckel-jax/bin/python main.py --N 50 --l 0 --batch_size 128 --job pred --beta exp 
+/h/rvargas/.conda/envs/huckel-jax/bin/python main.py --N 10 --lr 2E-2 --l 0 --batch_size 128 --job opt --beta exp -Wdecay h_x h_xy
+/h/rvargas/.conda/envs/huckel-jax/bin/python main.py --N 10 --l 0 --job pred --beta exp 
 
 
 
